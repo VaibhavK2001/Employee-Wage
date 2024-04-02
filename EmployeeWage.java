1a82f2cb5bcb5b8359bf853b8f1d4086174e8703
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-04-02 21:24:58 +0530

    Welcome Message

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,20 +4,8 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         
-        final int wagePerHour = 20;
-        final int fullDayHour = 8;
-        final int partTimeHour = 4;
-        
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM\n");
         
-        Random rand = new Random();
-        boolean isPresent = rand.nextBoolean();
-        if (isPresent) {
-            System.out.println("Employee is Present");
-        }else{
-            System.out.println("Employee is Absent");
-        }
-
     }
     
 }
