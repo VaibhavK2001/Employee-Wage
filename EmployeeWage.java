ad9327aa3c5df48e49433c8e96d887583a4ec7b3
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-04-03 07:50:34 +0530

    Wage for 100Hr & 20D

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -12,9 +12,13 @@ public class EmployeeWage {
         
         int totalWage = 0;
         int totalDays = 0;
-        Random random = new Random();
+        int totalHours = 0;
+        int full = 0;
+        int part = 0;
 
-        while (totalDays < 20) {
+        Random random = new Random();
+        
+        while (totalDays < 20 && totalHours < 100) {
             totalDays++;
             int attendance = random.nextInt(3);
             switch (attendance) {
@@ -22,17 +26,27 @@ public class EmployeeWage {
                     totalWage += 0;
                     break;
                 
-                case 1:
+                case 1:             // for full time
                     totalWage += fullDayHour * wagePerHour;
+                    totalHours += fullDayHour;
+                    full++;
                     break;
     
-                case 2:
+                case 2:             // for part time
                     totalWage += partTimeHour * wagePerHour;
+                    totalHours += partTimeHour;
+                    part++;
                     break;
             }
         }
         
-        System.out.println("Total Wage for 20 Days: "+totalWage);
+        System.out.println("Total Monthly Wage : $"+totalWage);
+        System.out.println("Total Full Time Days : "+full);
+        System.out.println("Total Part Time Days : "+part);
+        System.out.println("Total Absent Days : "+(20-(full+part)));
+        System.out.println("Total Working Days : "+totalDays);
+        System.out.println("Total Working Hours : "+totalHours);
+        System.out.println();
     }
     
 }
