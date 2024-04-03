5df9f6dc633bf78d96322d2160b92cc51837789d
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-04-03 16:23:30 +0530

    Wage for Multiple Company using Class

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,56 @@
 import java.util.Random;
 
+class CompanyEmpWage {
+
+    public final String company;
+    public final int wagePerHour;
+    public final int noOfDays;
+    public final int workingHours;
+    public int totalWage;
+
+    public CompanyEmpWage(String company, int wagePerHour, int noOfDays, int workingHours) {
+        this.company = company;
+        this.wagePerHour = wagePerHour;
+        this.noOfDays = noOfDays;
+        this.workingHours = workingHours;
+    }
+
+    public void setEmpWage(int totalWage){
+        this.totalWage = totalWage;
+    }
+
+    @Override
+    public String toString(){
+        return "Total Employee Wage for Company "+company+" is : "+totalWage+"\n";
+    }
+    
+}
+
 public class EmployeeWage {
 
-    static final int fullDayHour = 8;
-    static final int partTimeHour = 4;
+    int noOfCompany = 0;
+    CompanyEmpWage[] companyEmpWageArray;
+
     
-    public void calculateWage(String company,int wagePerHour, int noOfDays, int workingHours){
+    
+    public EmployeeWage() {
+        companyEmpWageArray = new CompanyEmpWage[5];
+    }
+
+    public void addCompanyEmpWage(String company, int wagePerHour, int noOfDays, int workingHours){
+        companyEmpWageArray[noOfCompany] = new CompanyEmpWage(company, wagePerHour, noOfDays, workingHours);
+        noOfCompany++;
+    }
+
+    public void computeWage(){
+        for (int i = 0; i < noOfCompany; i++) {
+            companyEmpWageArray[i].setEmpWage(this.calculateWage(companyEmpWageArray[i]));
+            System.out.println(companyEmpWageArray[i]);
+        }
+    }
+
+
+    public int calculateWage(CompanyEmpWage companyEmpWage){
         
         int totalWage = 0;
         int totalDays = 0;
@@ -15,7 +60,7 @@ public class EmployeeWage {
 
         Random random = new Random();
         
-        while (totalDays < noOfDays && totalHours < workingHours) {
+        while (totalDays < companyEmpWage.noOfDays && totalHours < companyEmpWage.workingHours) {
             totalDays++;
             int attendance = random.nextInt(3);
             switch (attendance) {
@@ -24,41 +69,36 @@ public class EmployeeWage {
                     break;
                 
                 case 1:             // for full time
-                    totalWage += fullDayHour * wagePerHour;
-                    totalHours += fullDayHour;
+                    totalWage += 8 * companyEmpWage.wagePerHour;
+                    totalHours += 8;
                     full++;
                     break;
     
                 case 2:             // for part time
-                    totalWage += partTimeHour * wagePerHour;
-                    totalHours += partTimeHour;
+                    totalWage += 4 * companyEmpWage.wagePerHour;
+                    totalHours += 4;
                     part++;
                     break;
             }
         }
-        
-        System.out.println("Company : "+company);
-        System.out.println("Total Monthly Wage : $"+totalWage);
+
         System.out.println("Total Full Time Days : "+full);
         System.out.println("Total Part Time Days : "+part);
-        System.out.println("Total Absent Days : "+(noOfDays-(full+part)));
+        System.out.println("Total Absent Days : "+(companyEmpWage.noOfDays-(full+part)));
         System.out.println("Total Working Days : "+totalDays);
         System.out.println("Total Working Hours : "+totalHours);
         System.out.println();
+        return totalWage;
  
     }
     public static void main(String[] args) {
 
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM\n");
         
-        EmployeeWage TATA = new EmployeeWage();
-        TATA.calculateWage("TATA",25,25,125);
-        
-        EmployeeWage RELIANCE = new EmployeeWage();
-        RELIANCE.calculateWage("RELIANCE",30,30,140);
-        
-        EmployeeWage QualtiyKiosk = new EmployeeWage();
-        QualtiyKiosk.calculateWage("QK",35,25,130);
+        EmployeeWage Obj1 = new EmployeeWage();
+        Obj1.addCompanyEmpWage("Samsung", 50, 25, 100);
+        Obj1.addCompanyEmpWage("Google", 40, 30, 120);
+        Obj1.computeWage();
     }
     
 }
